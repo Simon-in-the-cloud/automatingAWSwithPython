0a42dd68fae55d3b4a96e63cc14fada20038afaa
Simon Harvey <Simon@adynamic.cloud>
2019-07-13 14:59:40 +0200

    added boto3

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,2 +1,8 @@
+import boto3
+
+session = boto3.Session(profile_name='pythonAutomation')
+s3=session.resource('s3')
+
 if __name__=='__main__':
-    print("Hello Simon")
+    for bucket in s3.buckets.all():
+        print(bucket)
